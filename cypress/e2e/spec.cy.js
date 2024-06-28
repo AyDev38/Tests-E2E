59337d589b149a89943fa4e76ccae7c9dff28eb2
Matthieu <matthieu.fanget1@gmail.com>
2024-06-28 13:37:03 +0200

    Ajout du test inscription/connexion

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,3 +1,5 @@
+import 'cypress-file-upload';
+
 describe('Test de 0Bank', () => {
   beforeEach(() => {
     cy.visit('http://151.80.32.16:8965/')
@@ -84,21 +86,95 @@ describe('Test de connexion', () => {
   it("Test de connexion", () => {
     // Cliquez sur le lien de connexion dans la barre de navigation
     cy.get(".navbar-collapse ul li:last-child a").click();
-    
+
     // Remplissez le champ email et vérifiez la valeur
     cy.get("#mailco").type("test@test.com");
     cy.get("#mailco").should("have.value", "test@test.com");
-    
+
     // Remplissez le champ mot de passe et vérifiez la valeur
     cy.get("#connecttoaccount input[type=password]").type("test");
     cy.get("#connecttoaccount input[type=password]").should("have.value", "test");
-    
+
     // Cliquez sur le bouton de soumission dans le formulaire de connexion
     cy.get("#connecttoaccount .submit-button").should('be.visible').click();
-    
+
     // Vérifiez que l'URL inclut "/index.php" après la connexion
     cy.url().should("include", "/index.php");
   })
 })
 
+describe('Test du formulaire "Ouvrir mon compte"', () => {
+  beforeEach(() => {
+    cy.visit('http://151.80.32.16:8965/')
+  })
+  it('Test du formulaire "Ouvrir mon compte"', () => {
+    cy.get('.navbar-collapse ul li:nth-child(2) a').click();
+    cy.url().should('include', '#openaccount');
+
+    // Remplir le formulaire
+    cy.get('#openaccount input[name="lastname"]').type('Doe');
+    cy.get('#openaccount input[name="firstname"]').type('John');
+    cy.get('#openaccount input[name="born"]').type('1980-01-01');
+    cy.get('#openaccount input[name="adre"]').type('123 Main St');
+    cy.get('#openaccount input[name="pcode"]').type('75001');
+    cy.get('#openaccount input[name="pi"]').attachFile('../assets/imageTest.png'); // Chemin vers votre fichier d'image
+    cy.get('#openaccount input[name="mail"]').type('ben@intraneos-synergy.com'); // Utilisation de l'email souhaité
+    cy.get('#openaccount input[name="pass"]').type('password123');
+    cy.get('#openaccount input[name="passverif"]').type('password123');
+    cy.get('#openaccount input[name="cguvalid"]').check();
+
+    // Soumettre le formulaire
+    cy.get('#openaccount button[type="submit"]').click();
+
+    // Vérifier que la redirection ou la réponse attendue est correcte
+    cy.url().should('include', '/expected-redirect-url');
+    cy.contains('Merci pour votre inscription').should('be.visible'); // Adjustez selon le message de confirmation attendu
+  });
+});
+
+describe('Test inscription/connexion d\'un compte', () => {
+  beforeEach(() => {
+    cy.visit('http://151.80.32.16:8965/')
+  })
+  it('Test inscription/connexion d\'un compte', () => {
+    cy.get('.navbar-collapse ul li:nth-child(2) a').click();
+    cy.url().should('include', '#openaccount');
+
+    // Remplir le formulaire
+    cy.get('#openaccount input[name="lastname"]').type('Doe');
+    cy.get('#openaccount input[name="firstname"]').type('John');
+    cy.get('#openaccount input[name="born"]').type('1980-01-01');
+    cy.get('#openaccount input[name="adre"]').type('123 Main St');
+    cy.get('#openaccount input[name="pcode"]').type('75001');
+    cy.get('#openaccount input[name="pi"]').attachFile('../assets/imageTest.png'); // Chemin vers votre fichier d'image
+    cy.get('#openaccount input[name="mail"]').type('ben@intraneos-synergy.com'); // Utilisation de l'email souhaité
+    cy.get('#openaccount input[name="pass"]').type('password123');
+    cy.get('#openaccount input[name="passverif"]').type('password123');
+    cy.get('#openaccount input[name="cguvalid"]').check();
+
+    // Soumettre le formulaire
+    cy.get('#openaccount button[type="submit"]').click();
+
+    // Vérifier que la redirection ou la réponse attendue est correcte
+    cy.url().should('include', '/expected-redirect-url');
+    cy.contains('Merci pour votre inscription').should('be.visible'); // Adjustez selon le message de confirmation attendu
+
+    // Cliquez sur le lien de connexion dans la barre de navigation
+    cy.get(".navbar-collapse ul li:last-child a").click();
+
+    // Remplissez le champ email et vérifiez la valeur
+    cy.get("#mailco").type("test@test.com");
+    cy.get("#mailco").should("have.value", "test@test.com");
+
+    // Remplissez le champ mot de passe et vérifiez la valeur
+    cy.get("#connecttoaccount input[type=password]").type("test");
+    cy.get("#connecttoaccount input[type=password]").should("have.value", "test");
+
+    // Cliquez sur le bouton de soumission dans le formulaire de connexion
+    cy.get("#connecttoaccount .submit-button").should('be.visible').click();
+
+    // Vérifiez que l'URL inclut "/index.php" après la connexion
+    cy.url().should("include", "/index.php");
+  });
+})
 

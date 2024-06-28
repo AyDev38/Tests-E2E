52f112b82dddd5b99f02c5fd10ab12a2dbf2a6d9
Matthieu <matthieu.fanget1@gmail.com>
2024-06-28 11:35:17 +0200

    Commencement des tests

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,15 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Test de 0Bank', () => {
+  beforeEach(() => {
+    cy.visit('http://151.80.32.16:8965/')
+  })
+
+  it("Test de connexion", () => {
+    cy.get(".navbar-collapse ul li:last-child a").click()
+    cy.get("#mailco").type("test@test.com")
+    cy.get("#mailco").should("have.value", "test@test.com")
+    cy.get("#connecttoaccount input[type=password]").type("test")
+    cy.get("#connecttoaccount input[type=password]").should("have.value", "test")
+    cy.get("#connecttoaccount input[type=submit]").click()
+    cy.url().should("include", "/index.php")
   })
 })
